ba734c9a74e58ada7aa235cdf4c1ca9cb787a917
Alex Bromley <alexandrabromley@gmail.com>
2015-10-14 17:58:35 -0400

    updated file name and trying to upload again

---
+++ assignment5.js
@@ -0,0 +1,90 @@
+// A one line COMMENT has two forward slashes at the beginning
+/* this is where
+multi-line or block
+comments go */
+
+/* The CONSOLE is a built into the Chrome browser. It shows
+the javascript output the way it's being run by the browser.
+You can directly input javascript code to see the output and
+to check whether your code works. Click the hamburger menu
+and click more tools, then developer tools, then click the 
+console tab. */
+
+
+/* alert("Howdy");
+console.log("Howdy")
+An alert is a pop-up window that will be triggered when
+a page is accessed. It will prevent a user from using other 
+parts of a site until it is closed. Too many alerts can
+slow down a person's computer. */
+
+
+/* A variable stores or holds a value, which can be any data type, the
+four basic data types are: string which is a series of characters
+the string is always between quotes, number which is a numerical value starting from "0", 
+boolean data, which is true or false value, and undefined, which is no value. */
+
+/* var fruitName = "orange";
+console.log(fruitName);
+variable is fruitName and the value is orange
+orange is a STRING */
+
+/*  var x = 2;
+var y = 3;
+var b = x + y;
+console.log(b);
+'x' stores the value of 2, and 'y' stores a value of 3, and 
+'b' stores the valuable of "x" and "y" when added
+whatever the value of x and y may be
+the data type in this example is a NUMBER */
+
+/* var num1 = 1;
+var num2 = 2;
+console.log(num1 === num2);
+this is an example of a value comparison, which will always 
+return a BOOLEAN true or false. We use "===" because it will 
+check for both value and type. */
+
+/* An ARRAY is one variable that has multiple values stored, each value in an array has an index starting from 0,
+arrays can hold strings or numbers
+var fruits = ["apples", "grapes", "pears","bananas"];
+console.log(fruits [3]);
+the varable is fruits, there are 4 values for fruit.
+in this case bananas has an index of 3 */
+
+/* To test two strings of data you use comparison operators. Comparison operators include:
+===	equal value and equal type
+!==	not equal value or not equal type
+>=	greater than or equal to
+>=	greater than or equal to 
+
+You can test your javascript in the console using "console.log()" */
+
+/* Logic
+Controls the flow of program
+used to run code under certain conditions
+run code multiple times
+break down procedure into logical chunks
+Allows computer to do thinking */
+
+/* Functions
+A function is a procedure that performs a speciﬁc action. You define a function
+and then you call it. Defining a function is saying what you want it to do and 
+calling a function is telling the function to execute or run.
+A function has an argument that is an input specific to that function.
+A return value is the result or output after a function is run. A function
+is only run once and then it stops. */
+
+/* function alertPrize(doorNum) {
+if (doorNum ===1){
+alert('won a car');
+} else if (doorNum===2) {
+alert('you won a vacay');
+}
+
+Still very confused by functions. I think I get the concept on paper,
+but have difficulty putting them into action. For example I don't get
+why when I put in the above example, I can't get the alert to show in 
+console. */
+
+
